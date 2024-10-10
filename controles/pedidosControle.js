c578914f007b54d8dda62d9bb70987525c662fbc
miguelphb21 <miguel.s.silva9@aluno.senai.br>
2024-10-10 10:05:18 -0300

    adicao de um filtro

--- controles/pedidosControle.js
+++ controles/pedidosControle.js
@@ -21,26 +21,78 @@ const verPedido = async (req, res) => {
         [req.params.id], // Parâmetro para filtrar por cliente
         
         (err, results) => {
-            const {pedido_entregue} = req.body
+            
             if (err) {
                 console.error('Erro ao ver pedidos', err);
                 res.status(500).send('Erro ao buscar pedido');
             } 
-
-            const matriz = results
             
-            const pedidosNaoEntregue = matriz.map((valor)=>{
-                if (valor.pedido_entregue === 0){
-                    matriz.push(valor)
-                }
-                return matriz
-            })
-            res.json(pedidosNaoEntregue)
+            const matriz = results
+            const pedidosEmRota = []
 
+            function pedidosNaoEntregue(){
+                for (let i = 0; i <= matriz.length -1 ; i++){
+
+                    if (matriz[i].pedido_entregue === 0){
+                        pedidosEmRota.push(matriz[i])
+                    }
+
+                }
+                return pedidosEmRota
+            }
+
+            res.json(pedidosNaoEntregue())
 
         }
     );
 };
+
+// const verPedido = async (req, res) => {
+
+//     db.query(
+        
+//         `
+//         SELECT 
+//             clientes.nome, 
+//             carrinho.valor_total,
+//             livros.titulo,
+//             pedidos.pedido_entregue
+//         FROM 
+//             clientes
+//             INNER JOIN carrinho ON clientes.id = carrinho.id_cliente
+//             INNER JOIN livros ON carrinho.id_livro = livros.id
+//             INNER JOIN pedidos ON pedidos.pedido_entregue = pedidos.pedido_entregue
+//         WHERE clientes.id = ?;
+//         `,
+//         [req.params.id], // Parâmetro para filtrar por cliente
+        
+//         (err, results) => {
+            
+//             if (err) {
+//                 console.error('Erro ao ver pedidos', err);
+//                 res.status(500).send('Erro ao buscar pedido');
+//                 return
+//             } 
+            
+//             const matriz = results
+//             const pedidosEmRota = []
+
+//             function pedidosNaoEntregue(){
+//                 for (let i = 0; i <= matriz.length -1 ; i++){
+
+//                     if (matriz[i].pedido_entregue === 0){
+//                         pedidosEmRota.push(matriz[i])
+//                     }
+                    
+//                 }
+//                 return pedidosEmRota
+//             }
+
+//             res.json(pedidosNaoEntregue())
+
+//         }
+//     );
+// };
 
 module.exports = {
     verPedido
